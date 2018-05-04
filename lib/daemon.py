96fa651fba9d949299d50c448142755ecf1ec512
aitorp6 <aitorp@gmail.com>
2018-05-04 09:31:52 +0200

    Initial changes

--- lib/daemon.py
+++ lib/daemon.py
@@ -305,7 +305,7 @@ class Daemon(DaemonThread):
         gui_name = config.get('gui', 'qt')
         if gui_name in ['lite', 'classic']:
             gui_name = 'qt'
-        gui = __import__('electrum_gui.' + gui_name, fromlist=['electrum_deeponion_gui'])
+        gui = __import__('electrum_deeponion_gui.' + gui_name, fromlist=['electrum_deeponion_gui'])
         self.gui = gui.ElectrumGui(config, self, plugins)
         try:
             self.gui.main()
