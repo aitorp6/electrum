d9be73be2d29bf2410534b96ddf1329b92a8c967
aitorp6 <aitorp@gmail.com>
2018-05-04 09:17:53 +0200

    Initial changes

--- scripts/util.py
+++ scripts/util.py
@@ -1,8 +1,8 @@
 import select, time, queue
-# import electrum
-from electrum import Connection, Interface, SimpleConfig
+# import electrum_deeponion as electrum
+from electrum_deeponion import Connection, Interface, SimpleConfig
 
-from electrum.network import parse_servers
+from electrum_deeponion.network import parse_servers
 from collections import defaultdict
 
 # electrum.util.set_verbosity(1)
